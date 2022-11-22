08bd5dc3262b09475d53b17e981544cc829f1745
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-11-22 17:14:33 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Form } from './components/Form'
+import './App.css'
+import { ListCNAB } from './components/ListCNAB'
+
+
+const App = () => {
+
+	return (
+    	<div className="App">
+      		<Form />
+			<ListCNAB />
+    	</div>
+	)
+}
+
+export default App
---
+++ src/components/ListCNAB/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const ListCNAB = () => {
+
+    return (
+        <div>
+            <h1>List CNAB</h1>
+        </div>
+    )
+}
+
+export { ListCNAB }
